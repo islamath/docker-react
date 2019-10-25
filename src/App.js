b0a73fb0cdcd4cade62bdb77fcb10bbe2afc2f3e
Rahim Mohamed <mohamed.rahim@outlook.com>
2019-10-25 16:13:21 +0100

    change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          salam sa7bi
+          New Feature
         </p>
         <a
           className="App-link"
